3f2f2b340f8beefef510b90d6ce445b9f8623bb1
bryansbtian <bryan.sebastian189@gmail.com>
2025-09-28 10:44:29 -0400

    Add Order page + Inventory parity + DataContext wiring

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,46 +1,61 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Layout from '../Layout.jsx'
-import Dashboard from '../Pages/Dashboard'
-import Inventory from '../Pages/Inventory'
-import Weather from '../Pages/Weather'
-import Analytics from '../Pages/Analytics'
-import './index.css'
-import { DataProvider } from './context/DataContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "../Layout.jsx";
+import Dashboard from "../Pages/Dashboard";
+import Inventory from "../Pages/Inventory";
+import Order from "../Pages/Order";
+import Weather from "../Pages/Weather";
+import "./index.css";
+import { DataProvider } from "./context/DataContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <DataProvider>
       <Router>
         <Routes>
-          <Route path="/" element={
-            <Layout currentPageName="Dashboard">
-              <Dashboard />
-            </Layout>
-          } />
-          <Route path="/dashboard" element={
-            <Layout currentPageName="Dashboard">
-              <Dashboard />
-            </Layout>
-          } />
-          <Route path="/inventory" element={
-            <Layout currentPageName="Inventory">
-              <Inventory />
-            </Layout>
-          } />
-          <Route path="/weather" element={
-            <Layout currentPageName="Weather">
-              <Weather />
-            </Layout>
-          } />
-          <Route path="/analytics" element={
-            <Layout currentPageName="Analytics">
-              <Analytics />
-            </Layout>
-          } />
+          <Route
+            path="/"
+            element={
+              <Layout currentPageName="Dashboard">
+                <Dashboard />
+              </Layout>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <Layout currentPageName="Dashboard">
+                <Dashboard />
+              </Layout>
+            }
+          />
+          <Route
+            path="/inventory"
+            element={
+              <Layout currentPageName="Inventory">
+                <Inventory />
+              </Layout>
+            }
+          />
+          <Route
+            path="/order"
+            element={
+              <Layout currentPageName="Order">
+                <Order />
+              </Layout>
+            }
+          />
+          <Route
+            path="/weather"
+            element={
+              <Layout currentPageName="Weather">
+                <Weather />
+              </Layout>
+            }
+          />
         </Routes>
       </Router>
     </DataProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
